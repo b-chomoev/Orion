2b85b3727e4f8298c136bc703b87bf4708455e0c
Beksultan Chomoev <chomoevbeksultan@gmail.com>
2025-02-18 22:44:48 -0600

    User model is created and port is available to run now.

---
+++ api/models/User.ts
@@ -0,0 +1,88 @@
+import mongoose, {HydratedDocument, Model} from "mongoose";
+import {UserFields} from "../types";
+import {randomUUID} from "node:crypto";
+import bcrypt from 'bcrypt';
+
+interface UserMethods {
+    checkPassword(password: string): Promise<boolean>;
+    generateToken(): void;
+}
+
+type UserModel = Model<UserFields, {}, UserMethods>
+
+const Schema = mongoose.Schema;
+const SALT_WORK_FACTOR = 10;
+const regEmail = /^(\w+[-.]?\w+)@(\w+)+([.-]?\w+)?(\.[a-zA-Z]{2,3})$/;
+
+const UserSchema = new Schema<HydratedDocument<UserFields>, UserModel, UserMethods>({
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        validate: [{
+            validator: async function (this: HydratedDocument<UserFields>,value: string): Promise<boolean> {
+                if (!this.isModified('username')) return true;
+                const user: UserFields | null = await User.findOne({email: value});
+                return !user;
+            },
+            message: "This username is already taken",
+        }, {
+            validator: async function (this: HydratedDocument<UserFields>, value: string): Promise<boolean> {
+                return regEmail.test(value);
+            },
+            message: "Invalid email format",
+        }]
+    },
+    displayName: {
+        type: String,
+        required: true,
+    },
+    avatar: {
+        type: String,
+        required: true,
+        default: null,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    role: {
+        type: String,
+        required: true,
+        enum: ['user', 'admin'],
+        default: 'user',
+    },
+    token: {
+        type: String,
+        required: true,
+    }
+});
+
+UserSchema.pre('save', async function (next) {
+    if (!this.isModified('password')) return next();
+
+    const salt = await bcrypt.genSalt(SALT_WORK_FACTOR);
+    const hash = await bcrypt.hash(this.password, salt);
+
+    this.password = hash;
+    next();
+});
+
+UserSchema.methods.checkPassword = function (password: string) {
+    return bcrypt.compare(password, this.password);
+}
+
+UserSchema.methods.generateToken = function () {
+    this.token = randomUUID();
+}
+
+UserSchema.set('toJSON', {
+    transform: (doc, ret, options) => {
+        delete ret.password;
+        return ret;
+    }
+});
+
+const User = mongoose.model('User', UserSchema);
+
+export default User;
---
+++ api/types.d.ts
@@ -0,0 +1,8 @@
+export interface UserFields {
+    email: string;
+    displayName: string;
+    avatar: string | null;
+    password: string;
+    role: string;
+    token: string;
+}
